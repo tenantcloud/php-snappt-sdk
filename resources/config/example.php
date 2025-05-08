b034f594161cfa7dc364766e37cc95413781282c
ArturOlarTC <artur.olar@tenantcloud.com>
2025-05-08 11:40:45 +0300

    Initial commit

---
+++ resources/config/example.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+	'skeleton' => 'Skeleton',
+];
---
+++ src/SkeletonServiceProvider.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace TenantCloud\Skeleton;
+
+use Illuminate\Support\ServiceProvider;
+
+class SkeletonServiceProvider extends ServiceProvider
+{
+}
