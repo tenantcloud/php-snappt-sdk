4209692e39ca1403015b21dcd87a789e62e6e56b
ArturOlar <artur.olar@tenantcloud.com>
2025-07-01 12:17:17 +0300

    wip: Added undetermined_snappt fake email

--- src/Fake/FakeApplicantsApi.php
+++ src/Fake/FakeApplicantsApi.php
@@ -4,6 +4,7 @@ namespace TenantCloud\Snappt\Fake;
 
 use GuzzleHttp\Psr7\Response;
 use Illuminate\Contracts\Cache\Repository;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 use Psr\Http\Message\ResponseInterface;
 use TenantCloud\Snappt\Applicants\ApplicantsApi;
@@ -25,23 +26,38 @@ class FakeApplicantsApi implements ApplicantsApi
 			throw new NotFoundException();
 		}
 
-		$applicantDto = ApplicantDTO::from($this->cache->get("{$applicantId}:applicant"))
-			->setId($applicantId)
-			->setEntryId(Str::uuid()->toString())
-			->setInsertedAt('2025-05-27T08:21:46.000Z')
-			->setUpdatedAt('2025-05-27T08:21:46.000Z')
-			->setStatus('READY')
-			->setResult('CLEAN')
-			->setIncomeVerificationDetails(
-				IncomeVerificationDetailDTO::create()
-					->setCalculationDate('2025-05-27T08:32:00.262Z')
-					->setGrossDailyIncome('308.07')
-					->setGrossMonthlyIncome('9377.79')
-					->setGrossYearlyIncome('112533.46')
-					->setConsecutiveDays(31)
-					->setStatus('ok')
-					->setStatusDetails([])
-			);
+		$applicantData = $this->cache->get("{$applicantId}:applicant");
+
+		if (
+			Arr::get($applicantData, 'email') === 'undetermined_snappt@tenantcloud.com' &&
+			!Arr::get($applicantData, 'hasPreviouslySubmitted')
+		) {
+			$applicantDto = ApplicantDTO::from($applicantData)
+				->setId($applicantId)
+				->setEntryId(Str::uuid()->toString())
+				->setInsertedAt('2025-05-27T08:21:46.000Z')
+				->setUpdatedAt('2025-05-27T08:21:46.000Z')
+				->setStatus('READY')
+				->setResult('UNDETERMINED');
+		} else {
+			$applicantDto = ApplicantDTO::from($applicantData)
+				->setId($applicantId)
+				->setEntryId(Str::uuid()->toString())
+				->setInsertedAt('2025-05-27T08:21:46.000Z')
+				->setUpdatedAt('2025-05-27T08:21:46.000Z')
+				->setStatus('READY')
+				->setResult('CLEAN')
+				->setIncomeVerificationDetails(
+					IncomeVerificationDetailDTO::create()
+						->setCalculationDate('2025-05-27T08:32:00.262Z')
+						->setGrossDailyIncome('308.07')
+						->setGrossMonthlyIncome('9377.79')
+						->setGrossYearlyIncome('112533.46')
+						->setConsecutiveDays(31)
+						->setStatus('ok')
+						->setStatusDetails([])
+				);
+		}
 
 		$this->cache->put("{$applicantId}:applicant", [
 			...$applicantDto->toArray(),
@@ -53,7 +69,20 @@ class FakeApplicantsApi implements ApplicantsApi
 
 	public function report(string $applicantId, Preset $preset, Format $format): ResponseInterface
 	{
-		$content = (string) file_get_contents(__DIR__ . '/../../resources/reports/Sample Report_Clean with Income.pdf');
+		if (!$this->cache->has("{$applicantId}:applicant")) {
+			throw new NotFoundException();
+		}
+
+		$applicantData = $this->cache->get("{$applicantId}:applicant");
+
+		if (
+			Arr::get($applicantData, 'email') === 'undetermined_snappt@tenantcloud.com' &&
+			!Arr::get($applicantData, 'hasPreviouslySubmitted')
+		) {
+			$content = (string) file_get_contents(__DIR__ . '/../../resources/reports/Sample Report_Clean docs with Income Error.pdf');
+		} else {
+			$content = (string) file_get_contents(__DIR__ . '/../../resources/reports/Sample Report_Clean with Income.pdf');
+		}
 
 		return new Response(
 			200,
